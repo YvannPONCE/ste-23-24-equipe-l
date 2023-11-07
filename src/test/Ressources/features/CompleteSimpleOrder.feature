566b84ca524a8d7ff965e2ce1bdfce6db96d1587
Yvann PONCE <yvann.ponce@gmail.com>
2023-11-07 17:13:39 +0100

    implement complete single order

--- src/test/Ressources/features/CompleteSimpleOrder.feature
+++ src/test/Ressources/features/CompleteSimpleOrder.feature
@@ -5,5 +5,6 @@ Feature: Complete simple order
     Given user "maxime@gmail.com" order a "Maxibestof" at "Mcdonald" for 7.50 euros
     And "maxime@gmail.com" pay his order
     And The order is marked ready by the restaurant "Mcdonald"
-    When The delivery man deliver the order, "maxime@gmail.com" confirm the delivery
-    Then The order is marked as delivered
+    When The user "maxime@gmail.com" confirm the delivery
+    And The delivery man "deliver@gmail.com" confirm the delivery
+    Then The order is marked as closed
--- src/test/java/fr/unice/polytech/stEats/cucumber/CompleteSimpleOrder.java
+++ src/test/java/fr/unice/polytech/stEats/cucumber/CompleteSimpleOrder.java
@@ -2,11 +2,14 @@ package fr.unice.polytech.stEats.cucumber;
 
 import fr.unice.polytech.*;
 import fr.unice.polytech.Enum.Locations;
+import fr.unice.polytech.Enum.Status;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
 
 public class CompleteSimpleOrder {
 
@@ -14,9 +17,13 @@ public class CompleteSimpleOrder {
     OrderManager orderManager;
     RestaurantManager restaurantManager;
     Restaurant restaurant;
+    String user_email;
+
+    DeliveryManager deliveryManager;
 
     @Given("user {string} order a {string} at {string} for {double} euros")
     public void user_order_a_at_for_euros(String user_email, String menu_name, String restaurant_name, Double menu_price) {
+        this.user_email = user_email;
         restaurant = new Restaurant(restaurant_name);
         restaurantManager = new RestaurantManager();
         restaurantManager.add_restaurant(restaurant);
@@ -36,15 +43,23 @@ public class CompleteSimpleOrder {
     public void the_order_is_marked_ready_by_the_restaurant(String restaurant_name) {
         orderManager.validate_order(orderId, restaurant_name);
     }
-    @When("The delivery man deliver the order, {string} confirm the delivery")
-    public void the_delivery_man_deliver_the_order_confirm_the_delivery(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+    @When("The user {string} confirm the delivery")
+    public void the_user_confirm_the_delivery(String string) {
+        orderManager.validate_order_receipt(string,orderId);
     }
-    @Then("The order is marked as delivered")
+
+    @When("The delivery man {string} confirm the delivery")
+    public void the_delivery_man_confirm_the_delivery(String string) {
+        deliveryManager = new DeliveryManager(orderManager);
+        deliveryManager.addDeliveryman(string);
+        deliveryManager.addOrder(orderId);
+        deliveryManager.validateOrder(string,orderId);
+    }
+
+    @Then("The order is marked as closed")
     public void the_order_is_marked_as_delivered() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assertEquals(Status.CLOSED,orderManager.get_current_orders(orderId, user_email).get(0).getStatus());
     }
 
 }
