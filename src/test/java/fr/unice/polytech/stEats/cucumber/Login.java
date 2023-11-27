d0d103d45024327e16ff5c09e7b5d5ad279f3c9a
Mélanie Stanislas <melanie.stanislas@etu.univ-cotedazur.fr>
2023-11-27 19:47:24 +0100

    add scenario + cucumber squeleton

---
+++ src/test/java/fr/unice/polytech/stEats/cucumber/Login.java
@@ -0,0 +1,24 @@
+package fr.unice.polytech.stEats.cucumber;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Login {
+
+    @Given("the user with email {string} and a password of {string}")
+    public void the_user_with_email_and_a_password_of(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the user signin to the App")
+    public void the_user_signin_to_the_app() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the user can login and get the acess to orders")
+    public void the_user_can_login_and_get_the_acess_to_orders() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
