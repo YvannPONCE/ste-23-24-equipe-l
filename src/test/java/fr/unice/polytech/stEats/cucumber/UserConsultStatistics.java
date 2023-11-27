08a0465526663d624711e50d5ee69c774b279acb
Mélanie Stanislas <melanie.stanislas@etu.univ-cotedazur.fr>
2023-11-27 18:37:49 +0100

    add cucumber file squeleton

---
+++ src/test/java/fr/unice/polytech/stEats/cucumber/UserConsultStatistics.java
@@ -0,0 +1,29 @@
+package fr.unice.polytech.stEats.cucumber;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class UserConsultStatistics {
+
+    @Given("the application is running with two restaurant {string} and {string}")
+    public void the_application_is_running_with_two_restaurant_and(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("User {string} order a {string} at {int} euros at {string}")
+    public void user_order_a_at_euros_at(String string, String string2, Integer int1, String string3) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("when {string} consult her favorites restaurants")
+    public void when_consult_her_favorites_restaurants(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("{string} sees that her favorite restaurant is {string} following by {string}")
+    public void sees_that_her_favorite_restaurant_is_following_by(String string, String string2, String string3) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
