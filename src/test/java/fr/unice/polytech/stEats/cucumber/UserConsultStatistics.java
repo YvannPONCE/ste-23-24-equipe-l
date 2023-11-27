89e369565b71fb3b3354aabafef4b09178fd0ec6
Mélanie Stanislas <melanie.stanislas@etu.univ-cotedazur.fr>
2023-11-27 19:28:31 +0100

    add code for 2nd and 3rd scenario

--- src/test/java/fr/unice/polytech/stEats/cucumber/UserConsultStatistics.java
+++ src/test/java/fr/unice/polytech/stEats/cucumber/UserConsultStatistics.java
@@ -48,13 +48,44 @@ public class UserConsultStatistics {
     }
     @When("when {string} consult her favorites restaurants")
     public void when_consult_her_favorites_restaurants(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
     }
+
     @Then("{string} sees that her favorite restaurant is {string} following by {string}")
     public void sees_that_her_favorite_restaurant_is_following_by(String userEmail, String restaurantName1, String restaurantName2) {
         StatisticManagerStudent statisticsManagerStudent = statisticsManager;
         HashMap<String, Integer> favoriteRestaurant = statisticsManagerStudent.getFavoriteRestaurant(userEmail);
         Assert.assertTrue(favoriteRestaurant.get(restaurantName1) > favoriteRestaurant.get(restaurantName2));
+    }
+
+    @Given("the restaurant {string} has complete {int} orders to Polytech Hall")
+    public void the_restaurant_has_complete_orders_to_Polytech_Hall(String restaurantName, Integer numberOfOrders) {
+        Menu menu;
+        Order order;
+
+        for(int i =0;i<numberOfOrders;++i) {
+            menu = new Menu("Bucket", 7.5);
+            order = new Order(restaurantName, new ArrayList<Menu>(Arrays.asList(menu)));
+            orderManager.place_order("My email", order, Locations.HALL_PRINCIPAL);
+            orderManager.pay_user_orders("My email", "7936 3468 9302 8371");
+        }
+    }
+    @When("As a student I want to consult the most popular delivery locations")
+    public void as_a_student_i_want_to_consult_the_most_popular_delivery_locations() {
+    }
+
+    @Then("As a student I see that Polytech Hall has been choosen {int} times")
+    public void as_a_student_i_see_that_polytech_hall_has_been_choosen_times(Integer n) {
+        StatisticManagerStudent statisticManagerStudent = statisticsManager;
+        Assert.assertEquals(n, statisticManagerStudent.get_popular_locations().get(Locations.HALL_PRINCIPAL));
+    }
+
+    @When("when {string} consult her total number of orders")
+    public void when_consult_her_total_number_of_orders(String string) {
+    }
+
+    @Then("{string} sees that she made {int} orders")
+    public void sees_that_she_made_orders(String userEmail, Integer n) {
+        StatisticManagerStudent statisticManagerStudent = statisticsManager;
+        Assert.assertEquals(n.intValue(), statisticManagerStudent.getOrdersCount());
     }
 }
